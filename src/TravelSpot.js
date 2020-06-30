2e6d0fc3a6ccd69293301a0c4cebe49c970f38d9
Lexi <sakura9641@gmail.com>
2020-06-30 15:24:14 +0800

    add: 新增景點詳細資訊

--- src/TravelSpot.js
+++ src/TravelSpot.js
@@ -1,8 +1,32 @@
 import React, { useEffect, useCallback, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import { fetchSetting, clearSpotInfo } from './action/travelAction';
 const TravelSpot = () => {
-	let { id } = useParams();
+	const { id } = useParams();
+	const history=useHistory();
+	const dispatch = useDispatch();
+
+	const { travelListReducer, travelSpotReducer } = useSelector(
+		(state) => ({
+			travelListReducer: state.travelListReducer,
+			travelSpotReducer: state.travelSpotReducer
+		}),
+		shallowEqual
+	);
+	const { loading } = travelListReducer;
+	const { notFound, spotInfo } = travelSpotReducer;
+
+	useEffect(
+		() => {
+			dispatch(fetchSetting(travelListReducer.pageCount, id));
+
+			return()=>{
+				dispatch(clearSpotInfo())
+			}
+		},
+		[ dispatch, travelListReducer.pageCount, id ]
+	);
 
 	useEffect(
 		() => {
@@ -10,7 +34,41 @@ const TravelSpot = () => {
 		},
 		[ id ]
 	);
-	return <div>HELLO</div>;
+
+	const backToList=useCallback(()=>{
+		history.push('/');
+	}, [history])
+
+	if (notFound) return <div>NOT FOUND</div>;
+	if(loading) return <div>LOADING ....</div>
+	return (
+		<div>
+			<p>
+				<span onClick={()=> backToList()}>回列表</span>
+			</p>
+			<h1>{spotInfo.name}</h1>
+
+			<address>{spotInfo.address}</address>
+			<p>
+				<a href={spotInfo.official_site} rel="noopener noreferrer" target="_blank">
+					{spotInfo.official_site}
+				</a>
+			</p>
+			<p>
+				<a href={spotInfo.facebook} rel="noopener noreferrer" target="_blank">
+					{spotInfo.facebook}
+				</a>
+			</p>
+			<p>
+				<a href={spotInfo.url} rel="noopener noreferrer" target="_blank">
+					{spotInfo.url}
+				</a>
+			</p>
+
+			<div>{spotInfo.open_time}</div>
+			<div>{spotInfo.introduction}</div>
+		</div>
+	);
 };
 
 export default TravelSpot;
--- src/action/travelAction.js
+++ src/action/travelAction.js
@@ -1,6 +1,9 @@
 export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILED = 'FETCH_FAILED';
+export const SET_SPOT_INFO = 'SET_SPOT_INFO';
+export const NOT_FOUND = 'NOT_FOUND';
+export const CLEAR_SPOT_INFO = 'CLEAR_SPOT_INFO';
 
 export function fetchStart() {
 	return {
@@ -21,22 +24,22 @@ export function fetchFailed() {
 	};
 }
 
-export function fetchSetting(pageCount) {
-    const params = {};
-    if(pageCount) params.page = pageCount;
+export function fetchSetting(pageCount, id) {
+	const params = {};
+	if (pageCount) params.page = pageCount;
 
-    const urlParams = new URLSearchParams(Object.entries(params));
+	const urlParams = new URLSearchParams(Object.entries(params));
 
 	return async function(dispatch) {
 		dispatch(fetchStart());
 
-		await fetch('https://travelwebapi.azurewebsites.net//api/TaipeiTravel/zh-tw/Attractions/All?'+ urlParams, {
+		await fetch('https://travelwebapi.azurewebsites.net//api/TaipeiTravel/zh-tw/Attractions/All?' + urlParams, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
 				'Access-Control-Allow-Origin': '*',
 				Accept: 'application/json'
-            },
+			}
 		})
 			.then((data) => {
 				// console.log(data);
@@ -44,10 +47,26 @@ export function fetchSetting(pageCount) {
 			})
 			.then((res) => {
 				dispatch(fetchSuccess(res));
+				if(id) {
+					const infoIndex = res.data.map(spot=> spot.id).indexOf(parseInt(id, 10))
+					if(infoIndex!== -1){
+						const info = res.data[infoIndex]
+						dispatch({type: SET_SPOT_INFO,info})
+					}else{
+						dispatch({type: NOT_FOUND})
+					}
+				}
+
 			})
 			.catch((err) => {
 				dispatch(fetchFailed());
 				console.log('err---', err);
 			});
 	};
+}
+
+export function clearSpotInfo(){
+	return{
+		type: CLEAR_SPOT_INFO
+	}
 }
