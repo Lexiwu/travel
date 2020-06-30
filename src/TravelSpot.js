4f8f962e0a8c236d07f10737c01b7d3e9205c6a9
Lexi <sakura9641@gmail.com>
2020-06-30 19:06:26 +0800

    adjust: 景點詳細頁css

--- src/TravelSpot.js
+++ src/TravelSpot.js
@@ -2,9 +2,12 @@ import React, { useEffect, useCallback, useState } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { fetchSetting, clearSpotInfo } from './action/travelAction';
+import './style/travel.scss';
+import View from './static/image/view.jpg';
+
 const TravelSpot = () => {
 	const { id } = useParams();
-	const history=useHistory();
+	const history = useHistory();
 	const dispatch = useDispatch();
 
 	const { travelListReducer, travelSpotReducer } = useSelector(
@@ -21,9 +24,9 @@ const TravelSpot = () => {
 		() => {
 			dispatch(fetchSetting(id));
 
-			return()=>{
-				dispatch(clearSpotInfo())
-			}
+			return () => {
+				dispatch(clearSpotInfo());
+			};
 		},
 		[ dispatch, id ]
 	);
@@ -35,38 +38,60 @@ const TravelSpot = () => {
 		[ id ]
 	);
 
-	const backToList=useCallback(()=>{
-		history.push('/');
-	}, [history])
+	const backToList = useCallback(
+		() => {
+			history.push('/');
+		},
+		[ history ]
+	);
 
 	if (notFound) return <div>NOT FOUND</div>;
-	if(loading) return <div>LOADING ....</div>
+	if (loading) return <div>LOADING ....</div>;
 	return (
-		<div>
+		<div className="TravelSpot">
 			<p>
-				<span onClick={()=> backToList()}>回列表</span>
+				<span onClick={() => backToList()}>回列表</span>
 			</p>
-			<h1>{spotInfo.name}</h1>
+			<div className="TravelSpotInfo">
+				<h1>{spotInfo.name}</h1>
+				<figure>
+					<img src={View} alt="" />
+				</figure>
+				<div className="infoRow">
+					<div className="infoRow__title">地址：</div>
+					<div className="infoRow__info">
+						<address>{spotInfo.address}</address>
+					</div>
+				</div>
+				<div className="infoRow">
+					<div className="infoRow__title">官網：</div>
+					<div className="infoRow__info">
+						<a href={spotInfo.official_site} rel="noopener noreferrer" target="_blank">
+							{spotInfo.official_site}
+						</a>
+					</div>
+				</div>
 
-			<address>{spotInfo.address}</address>
-			<p>
-				<a href={spotInfo.official_site} rel="noopener noreferrer" target="_blank">
-					{spotInfo.official_site}
-				</a>
-			</p>
-			<p>
-				<a href={spotInfo.facebook} rel="noopener noreferrer" target="_blank">
-					{spotInfo.facebook}
-				</a>
-			</p>
-			<p>
-				<a href={spotInfo.url} rel="noopener noreferrer" target="_blank">
-					{spotInfo.url}
-				</a>
-			</p>
+				<div className="infoRow">
+					<div className="infoRow__title">FaceBook：</div>
+					<div className="infoRow__info">
+						<a href={spotInfo.facebook} rel="noopener noreferrer" target="_blank">
+							{spotInfo.facebook}
+						</a>
+					</div>
+				</div>
+				<div className="infoRow">
+					<div className="infoRow__title">詳細資訊：</div>
+					<div className="infoRow__info">
+						<a href={spotInfo.url} rel="noopener noreferrer" target="_blank">
+							{spotInfo.url}
+						</a>
+					</div>
+				</div>
 
-			<div>{spotInfo.open_time}</div>
-			<div>{spotInfo.introduction}</div>
+				<div className="openTime">{spotInfo.open_time}</div>
+				<div className="introduction">{spotInfo.introduction}</div>
+			</div>
 		</div>
 	);
 };
