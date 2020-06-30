98300de8c171df30f9064d4436a64e542a95f3b4
Jaja <a926a926@gmail.com>
2020-06-30 21:06:23 +0800

    adjust: 調整景點詳細頁樣式

--- src/TravelSpot.js
+++ src/TravelSpot.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback, useState } from 'react';
+                                                                        import React, { useEffect, useCallback, useState } from 'react';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { fetchSetting, clearSpotInfo } from './action/travelAction';
@@ -50,47 +50,57 @@ const TravelSpot = () => {
 	return (
 		<div className="TravelSpot">
 			<p>
-				<span onClick={() => backToList()}>回列表</span>
+				<button onClick={() => backToList()}>回列表</button>
 			</p>
 			<div className="TravelSpotInfo">
 				<h1>{spotInfo.name}</h1>
 				<figure>
 					<img src={View} alt="" />
 				</figure>
-				<div className="infoRow">
-					<div className="infoRow__title">地址：</div>
-					<div className="infoRow__info">
-						<address>{spotInfo.address}</address>
-					</div>
-				</div>
-				<div className="infoRow">
-					<div className="infoRow__title">官網：</div>
-					<div className="infoRow__info">
-						<a href={spotInfo.official_site} rel="noopener noreferrer" target="_blank">
-							{spotInfo.official_site}
-						</a>
-					</div>
-				</div>
 
-				<div className="infoRow">
-					<div className="infoRow__title">FaceBook：</div>
-					<div className="infoRow__info">
-						<a href={spotInfo.facebook} rel="noopener noreferrer" target="_blank">
-							{spotInfo.facebook}
-						</a>
+				<div className="TravelSpotInfoDetail">
+					<div className="infoRow">
+						<div className="infoRow__title">地址：</div>
+						<div className="infoRow__info">
+							<address>{spotInfo.address}</address>
+						</div>
 					</div>
-				</div>
-				<div className="infoRow">
-					<div className="infoRow__title">詳細資訊：</div>
-					<div className="infoRow__info">
-						<a href={spotInfo.url} rel="noopener noreferrer" target="_blank">
-							{spotInfo.url}
-						</a>
+					<div className="infoRow">
+						<div className="infoRow__title">官網：</div>
+						<div className="infoRow__info">
+							<a href={spotInfo.official_site} rel="noopener noreferrer" target="_blank">
+								{spotInfo.official_site}
+							</a>
+						</div>
 					</div>
-				</div>
 
-				<div className="openTime">{spotInfo.open_time}</div>
-				<div className="introduction">{spotInfo.introduction}</div>
+					<div className="infoRow">
+						<div className="infoRow__title">FaceBook：</div>
+						<div className="infoRow__info">
+							<a href={spotInfo.facebook} rel="noopener noreferrer" target="_blank">
+								{spotInfo.facebook}
+							</a>
+						</div>
+					</div>
+					<div className="infoRow">
+						<div className="infoRow__title">詳細資訊：</div>
+						<div className="infoRow__info">
+							<a href={spotInfo.url} rel="noopener noreferrer" target="_blank">
+								{spotInfo.url}
+							</a>
+						</div>
+					</div>
+
+					<div className="infoRow">
+						<div className="infoRow__title">營業時間：</div>
+						<div className="infoRow__info">
+							<div className="openTime">{spotInfo.open_time}</div>
+						</div>
+					</div>
+					
+					<div className="introduction">{spotInfo.introduction}</div>
+				
+				</div>		
 			</div>
 		</div>
 	);
